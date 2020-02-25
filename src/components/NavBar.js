7888805ca4574f5316e8018a8699237d8f7e339d
Michael Chen <michaelchengaming@gmail.com>
2020-02-25 13:05:13 -0800

    move styled navs into its own directory

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,61 +1,46 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-// import LogInForm from "./LogInForm";
+import LogInForm from "./LogInForm";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import styled from "styled-components";
+import { StyledNav } from "../styles/styledComponents";
 
-const Styles = styled.div`
-  .navbar {
-    background-color: #222;
-  }
-  .navbar-brand,
-  .navbar-nav .nav-link {
-    color: #bbb;
+const NavBar = () => (
+  //   <Navbar bg="dark" variant="dark" expand="md">
+  //   <Navbar.Brand href="/">LFG Collab</Navbar.Brand>
+  //   <Nav className="mr-auto">
+  //   <Nav.Link href="/">Home</Nav.Link>
+  //   <Nav.Link href="/projects">Projects</Nav.Link>
+  //   <Nav.Link href="/myprojects">My Projects</Nav.Link>
+  // </Nav>
+  //     <Form inline>
+  //       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+  //       <Button variant="outline-info">Search</Button>
+  //     </Form>
+  //     <LogInForm />
+  //     </Navbar>
 
-    &:hover {
-      color: white;
-    }
-  }
-`;
-
-const NavBar = () => {
-  return (
-    //   <Navbar bg="dark" variant="dark" expand="md">
-    //   <Navbar.Brand href="/">LFG Collab</Navbar.Brand>
-    //   <Nav className="mr-auto">
-    //   <Nav.Link href="/">Home</Nav.Link>
-    //   <Nav.Link href="/projects">Projects</Nav.Link>
-    //   <Nav.Link href="/myprojects">My Projects</Nav.Link>
-    // </Nav>
-    //     <Form inline>
-    //       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-    //       <Button variant="outline-info">Search</Button>
-    //     </Form>
-    //     <LogInForm />
-    //     </Navbar>
-
-    <Styles>
-      <Navbar expand="lg">
-        <Navbar.Brand hred="/"> LFG Collab </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Item>
-              <Nav.Link href="/">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="/projects">Projects</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="/myprojects">My Projects</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </Styles>
-  );
-};
+  <StyledNav>
+    <Navbar expand="lg">
+      <Navbar.Brand hred="/"> LFG Collab </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item>
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/projects">Projects</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/myprojects">My Projects</Nav.Link>
+          </Nav.Item>
+          <LogInForm />
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </StyledNav>
+);
 
 export default NavBar;
 
---
+++ src/styles/styledComponents.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const StyledNav = styled.div`
+  .navbar {
+    background-color: #222;
+  }
+  .navbar-brand,
+  .navbar-nav .nav-link {
+    color: #bbb;
+
+    &:hover {
+      color: white;
+    }
+  }
+`;
+
+export { StyledNav };
