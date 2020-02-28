1ff5f85d1712cc95233f5c841706fe1a7584bb27
Michael Chen <michaelchengaming@gmail.com>
2020-02-27 20:10:58 -0800

    change projects component from functional to classical

--- src/containers/ProjectContainer.js
+++ src/containers/ProjectContainer.js
@@ -1,37 +1,85 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+// import Project from "../components/project/Project";
+
+// const Projects = () => {
+//   const [projects, setProjects] = useState([]);
+
+//   useEffect(() => {
+//     fetchProjects();
+
+//     return () => {};
+//   }, []);
+
+//   const fetchProjects = async () => {
+//     const res = await fetch("http://localhost:3001/api/v1/projects/");
+//     const data = await res.json();
+//     console.log(data);
+//     setProjects(data);
+//     //
+//   };
+//   return (
+//     <div>
+//       {projects.map((project, index) => {
+//         return (
+//           <Project
+//             key={index}
+//             name={project.name}
+//             description={project.description}
+//             users={project.users}
+//             addedOn={project.created_at}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+// export default Projects;
+
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import Project from "../components/project/Project";
 
-const Projects = () => {
-  const [projects, setProjects] = useState([]);
+export class ProjectContainer extends Component {
+  state = {
+    projects: []
+  };
 
-  useEffect(() => {
-    fetchProjects();
-
-    return () => {};
-  }, []);
-
-  const fetchProjects = async () => {
+  fetchProjects = async () => {
     const res = await fetch("http://localhost:3001/api/v1/projects/");
     const data = await res.json();
     console.log(data);
-    setProjects(data);
-    //
+    this.setState({
+      projects: data
+    });
   };
-  return (
-    <div>
-      {projects.map((project, index) => {
-        return (
-          <Project
-            key={index}
-            name={project.name}
-            description={project.description}
-            users={project.users}
-            addedOn={project.created_at}
-          />
-        );
-      })}
-    </div>
-  );
-};
 
-export default Projects;
+  componentDidMount() {
+    this.fetchProjects();
+  }
+
+  render() {
+    const { projects } = this.state;
+    return (
+      <div>
+        {projects.map((project, index) => {
+          return (
+            <Project
+              key={index}
+              name={project.name}
+              description={project.description}
+              users={project.users}
+              addedOn={project.created_at}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectContainer);
