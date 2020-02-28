762f1a85601444aeb33e79c7c68a2f9774a9e1cc
Michael Chen <michaelchengaming@gmail.com>
2020-02-28 00:05:58 -0800

    add a try catch block to fetch projects

--- src/actions/fetchProjects.js
+++ src/actions/fetchProjects.js
@@ -2,9 +2,13 @@ import { LOADING_PROJECTS, ADD_PROJECTS } from "./actionTypes";
 
 export const fetchProjects = () => {
   return async dispatch => {
-    dispatch({ type: LOADING_PROJECTS });
-    const res = await fetch("http://localhost:3001/api/v1/projects/");
-    const data = await res.json();
-    dispatch({ type: ADD_PROJECTS, payload: data });
+    try {
+      dispatch({ type: LOADING_PROJECTS });
+      const res = await fetch("http://localhost:3001/api/v1/projects/");
+      const data = await res.json();
+      dispatch({ type: ADD_PROJECTS, payload: data });
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 };
