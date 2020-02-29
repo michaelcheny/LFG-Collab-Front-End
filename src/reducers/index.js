3d016394f9082625a706951b8f8069add30cb04e
Michael Chen <michaelchengaming@gmail.com>
2020-02-28 21:12:01 -0800

    add userReducer to combinedreducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import projectsReducer from "./projectsReducer";
 import tokenReducer from "./tokenReducer";
+import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
   projects: projectsReducer,
-  token: tokenReducer
+  token: tokenReducer,
+  user: userReducer
 });
 
 export default rootReducer;
