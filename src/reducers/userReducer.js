430e8c258a55f82e164b5e6de2fab675233daf7b
Michael Chen <michaelchengaming@gmail.com>
2020-02-28 21:02:44 -0800

    add log out and stuff to action types, add user reducer

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,37 @@
+import { LOGGING_IN, LOG_IN, LOG_OUT } from "../actions/actionTypes";
+
+const userReducer = (
+  state = {
+    user: {},
+    authenticated: false,
+    loading: false
+  },
+  action
+) => {
+  switch (action.type) {
+    case LOGGING_IN:
+      return {
+        ...state,
+        loading: true
+      };
+    case LOG_IN:
+      return {
+        ...state,
+        user: {
+          id: action.payload.id,
+          name: action.payload.name,
+          email: action.payload.email
+        },
+        loading: false
+      };
+    case LOG_OUT:
+      return {
+        ...state,
+        user: {}
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
