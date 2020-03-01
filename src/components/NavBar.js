d6c73523b076caca83514fe979d197cd9bd7e9eb
Michael Chen <michaelchengaming@gmail.com>
2020-02-29 23:47:19 -0800

    renders the links based on if authorized

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,62 +6,38 @@ import Navbar from "react-bootstrap/Navbar";
 import { StyledNav } from "../styles/styledComponents";
 
 const NavBar = ({ authenticated }) => {
-  //   <Navbar bg="dark" variant="dark" expand="md">
-  //   <Navbar.Brand href="/">LFG Collab</Navbar.Brand>
-  //   <Nav className="mr-auto">
-  //   <Nav.Link href="/">Home</Nav.Link>
-  //   <Nav.Link href="/projects">Projects</Nav.Link>
-  //   <Nav.Link href="/myprojects">My Projects</Nav.Link>
-  // </Nav>
-  //     <Form inline>
-  //       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-  //       <Button variant="outline-info">Search</Button>
-  //     </Form>
-  //     <LogInForm />
-  //     </Navbar>
-
-  // <StyledNav>
-  //   <Navbar expand="lg">
-  //     <Navbar.Brand hred="/"> LFG Collab </Navbar.Brand>
-  //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  //     <Navbar.Collapse id="basic-navbar-nav">
-  //       <Nav className="ml-auto">
-  //         <Nav.Item>
-  //           <Nav.Link href="/">Home</Nav.Link>
-  //         </Nav.Item>
-
-  //         <Nav.Item>
-  //           <Nav.Link href="/projects">Projects</Nav.Link>
-  //         </Nav.Item>
-
-  //         <Nav.Item>
-  //           <Nav.Link href="/myprojects">My Projects</Nav.Link>
-  //         </Nav.Item>
-
-  //         <Nav.Item>
-  //           <Nav.Link href="/registration">Register</Nav.Link>
-  //         </Nav.Item>
-
-  //         <LogInForm />
-  //       </Nav>
-  //     </Navbar.Collapse>
-  //   </Navbar>
-  // </StyledNav>
-
   const renderAuthLinks = () => {
-    return (
-      <>
-        <Nav.Item>
-          <Link to="/registration">
-            <li className="nav-routes">Register</li>
-          </Link>
-        </Nav.Item>
+    if (authenticated) {
+      return (
+        <>
+          <Nav.Item>
+            <Link to="/myprojects">
+              <li className="nav-routes">My Projects</li>
+            </Link>
+          </Nav.Item>
 
-        <li className="nav-routes">
-          <LogInForm />
-        </li>
-      </>
-    );
+          <Nav.Item>
+            <Link to="/account">
+              <li className="nav-routes">My Profile</li>
+            </Link>
+          </Nav.Item>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Nav.Item>
+            <Link to="/registration">
+              <li className="nav-routes">Register</li>
+            </Link>
+          </Nav.Item>
+
+          <li className="nav-routes">
+            <LogInForm />
+          </li>
+        </>
+      );
+    }
   };
 
   return (
@@ -71,7 +47,6 @@ const NavBar = ({ authenticated }) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            {/* <nav> */}
             <Nav.Item>
               <Link to="/">
                 <li className="nav-routes">Home</li>
@@ -84,40 +59,7 @@ const NavBar = ({ authenticated }) => {
               </Link>
             </Nav.Item>
 
-            <Nav.Item>
-              <Link to="/myprojects">
-                <li className="nav-routes">My Projects</li>
-              </Link>
-            </Nav.Item>
-
-            {/* <Nav.Item>
-                <Nav.Link>Home</Nav.Link>
-              </Nav.Item> */}
-
-            {/* <ul className="nav-links">
-                <Link to="/projects">
-                  <li className="nav-routes">Projects</li>
-                </Link> */}
-
-            {/* <Link to="/myprojects">
-                  <li className="nav-routes">My Projects</li>
-                </Link> */}
-
-            {authenticated ? (
-              <Nav.Item>
-                <Link to="/account">
-                  <li className="nav-routes">My Profile</li>
-                </Link>
-              </Nav.Item>
-            ) : (
-              renderAuthLinks()
-            )}
-
-            {/* <li className="nav-routes">
-          <LogInForm />
-        </li> */}
-            {/* </ul> */}
-            {/* </nav> */}
+            {renderAuthLinks()}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
