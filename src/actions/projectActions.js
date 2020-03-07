dec783df8f28e50b906b5ca150a9b3cca46991ff
Michael Chen <michaelchengaming@gmail.com>
2020-03-06 21:32:34 -0800

    correctly posts to the backend and creates a new project

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -39,9 +39,32 @@ export const fetchPersonalProjects = () => {
 };
 
 export const createProject = (token, project) => {
+  const { name, description, category } = project;
   return async dispatch => {
     try {
       console.log(project);
+      console.log(token);
+      const res = await fetch("http://localhost:3001/api/v1/projects", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "X-CSRF-TOKEN": token
+        },
+        body: JSON.stringify({
+          project: {
+            name,
+            description
+          },
+          category
+        }),
+        credentials: "include"
+      });
+      if (!res.ok) {
+        throw res;
+      }
+      const data = await res.json();
+      // do something hereeeee
     } catch (error) {
       console.log(error.message);
     }
