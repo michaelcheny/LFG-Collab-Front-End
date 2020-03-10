86d3306909637a16719c27d1e8ba7a579074d6d2
Michael Chen <michaelchengaming@gmail.com>
2020-03-10 11:00:04 -0700

    able to filter the selected categories now

--- src/components/project/CategorySelector.js
+++ src/components/project/CategorySelector.js
@@ -2,17 +2,25 @@ import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 
-export const CategorySelector = () => {
+export const CategorySelector = ({ changeCategory }) => {
   return (
-    <Dropdown onSelect={eventKey => console.log(eventKey)}>
-      <Dropdown.Toggle variant="dark" id="dropdown-basic">
-        Dropdown Button
+    <Dropdown onSelect={eventKey => changeCategory(eventKey)}>
+      <Dropdown.Toggle variant="dark" id="dropdown-basic" size="sm">
+        Category
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-        <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-        <Dropdown.Item eventKey="3">Something else</Dropdown.Item>
+        <Dropdown.Item eventKey="all">All</Dropdown.Item>
+        <Dropdown.Item eventKey="1">Automotive</Dropdown.Item>
+        <Dropdown.Item eventKey="2">Charity</Dropdown.Item>
+        <Dropdown.Item eventKey="3">Educational</Dropdown.Item>
+        <Dropdown.Item eventKey="4">Fashion</Dropdown.Item>
+        <Dropdown.Item eventKey="5">Food and Drink</Dropdown.Item>
+        <Dropdown.Item eventKey="6">Health and Fitness</Dropdown.Item>
+        <Dropdown.Item eventKey="7">Makers and Crafting</Dropdown.Item>
+        <Dropdown.Item eventKey="8">Programming</Dropdown.Item>
+        <Dropdown.Item eventKey="9">Social Media</Dropdown.Item>
+        <Dropdown.Item eventKey="10">Others</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
--- src/containers/ProjectsPage.js
+++ src/containers/ProjectsPage.js
@@ -6,35 +6,67 @@ import Spinner from "react-bootstrap/Spinner";
 import { CategorySelector } from "../components/project/CategorySelector";
 
 class ProjectPage extends Component {
+  state = {
+    categoryId: null
+  };
+
   componentDidMount() {
     this.props.fetchProjects();
   }
 
-  render() {
+  renderProjects = () => {
     const { projects, loading } = this.props;
+    if (!loading) {
+      if (this.state.categoryId) {
+        const selectedCategory = projects.filter(project => {
+          return project.category_id.toString() === this.state.categoryId;
+        });
+        USE TERNARY TO SELECT WHICH THING TO MAP if all or null then projects, else selectedCategory
+        return selectedCategory.map(project => {
+          return (
+            <Project
+              key={project.id}
+              name={project.name}
+              description={project.description}
+              users={project.users}
+              addedOn={project.created_at}
+              category={project.category.name}
+            />
+          );
+        });
+      } else {
+        return projects.map(project => {
+          return (
+            <Project
+              key={project.id}
+              name={project.name}
+              description={project.description}
+              users={project.users}
+              addedOn={project.created_at}
+              category={project.category.name}
+            />
+          );
+        });
+      }
+    } else {
+      return (
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      );
+    }
+  };
+
+  render() {
     return (
       <div>
         <h1>Available Projects</h1>
-        <CategorySelector />
+        <CategorySelector
+          changeCategory={id => this.setState({ categoryId: id })}
+        />
+        {console.log(this.state.categoryId)}
         <br />
-        {!loading ? (
-          projects.map((project, index) => {
-            return (
-              <Project
-                key={index}
-                name={project.name}
-                description={project.description}
-                users={project.users}
-                addedOn={project.created_at}
-                category={project.category.name}
-              />
-            );
-          })
-        ) : (
-          <Spinner animation="border" role="status">
-            <span className="sr-only">Loading...</span>
-          </Spinner>
-        )}
+        {this.renderProjects()}
       </div>
     );
   }
