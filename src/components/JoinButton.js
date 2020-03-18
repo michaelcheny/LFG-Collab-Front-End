8dbf05ebaec0bdcab55563c3fd6b2f231ef9df94
Michael Chen <michaelchengaming@gmail.com>
2020-03-18 14:52:46 -0700

    button renders correctly and is disabled correctly under circumstances

--- src/components/JoinButton.js
+++ src/components/JoinButton.js
@@ -2,44 +2,43 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Button from "react-bootstrap/Button";
 import { joinProject } from "../actions/projectActions";
-let joined = false;
+// let joined = false;
 class JoinButton extends Component {
   state = {
     joined: false
   };
 
-  renderButtonText = () => {
-    const { project, userId } = this.props;
+  renderButton = () => {
+    const { project, userId, token, authenticated, joinProject } = this.props;
     if (project.users) {
       let pp = project.users.filter(project => project.id === userId);
       console.log(pp);
       if (pp.length > 0) {
-        // this.setState({ joined: true });
-        joined = true;
-        return "Already Joined";
+        return (
+          <Button variant="dark" size="sm" disabled>
+            Already Joined
+          </Button>
+        );
       } else {
-        return "Join Project";
+        return (
+          <Button
+            variant="dark"
+            size="sm"
+            onClick={() => {
+              if (window.confirm("Are you sure you want to join this project?"))
+                joinProject(token, project.id);
+            }}
+            disabled={!authenticated}
+          >
+            Join Project
+          </Button>
+        );
       }
     }
   };
 
   render() {
-    const { project, token, authenticated, joinProject, userId } = this.props;
-
-    return (
-      <Button
-        variant="dark"
-        size="sm"
-        onClick={() => {
-          if (window.confirm("Are you sure you want to join this project?"))
-            joinProject(token, project.id);
-        }}
-        disabled={!authenticated || !joined}
-      >
-        {/* Join Project */}
-        {this.renderButtonText()}
-      </Button>
-    );
+    return <>{this.renderButton()}</>;
   }
 }
 
